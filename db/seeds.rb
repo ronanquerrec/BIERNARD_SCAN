83e60b8fca48ab734bbd562147d13740536d9cba
Hadrien Dalla Torre <hadrien.dallatorre@gmail.com>
2019-11-26 16:03:44 +0100

    proper seeding :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,15 @@
 puts 'Cleaning database...'
 BeerFlavour.destroy_all
+Favourite.destroy_all
 Flavour.destroy_all
 User.destroy_all
 Beer.destroy_all
 
-
-# malt = Flavour.create!(name: 'malt')
-# caramel = Flavour.create!(name: 'caramel')
-
-
-# beer = Beer.create!(
-#   name: "Boont Amber Ale",
-#   description: "La bière Boont Amber Ale de la brasserie Anderson Valley Brewing Co., Etats-Unis.",
-#   fizzing: 51,
-#   bitterness: 40,
-#   sweetness: 40,
-#   alcohol_percentage: 5.8,
-#   brewery: "Anderson Valley Brewing Co.",
-#   country: "Etats-Unis",
-#   url_image: "https://media.biernard.com/bieres/temp/1996-13880-v4_product_img.jpg",
-#   style: "Amber Ale",
-#   strength: 10,
-#   sourness: 6.7,
-#   colour: "Amber"
-# )
-
 puts 'Creating users...'
-User.create!(
+user = User.create!(
   email: 'jean@beer.com',
   password: "123456"
 )
-
-# BeerFlavour.create!(
-#   beer: beer,
-#   flavour: malt
-# )
-
-# BeerFlavour.create!(
-#   beer: beer,
-#   flavour: caramel
-# )
-
-
 
 require 'json'
 
@@ -72,9 +40,7 @@ beers["beers"].each do |beer|
   )
   # array of strings
   beer["flavors_tags"].each do |beer_flavor_tag|
-    # first iteration: beer_flavor_tag = "malt"
-    # first iteration: beer_flavor_tag = "caramel"
-    # 1.
+
     flavour = Flavour.find_or_create_by!(name: beer_flavor_tag)
     BeerFlavour.create!(
       beer: beer_record,
@@ -82,6 +48,14 @@ beers["beers"].each do |beer|
     )
   end
 
+end
+
+puts "Creating favorites beers list..."
+10.times do
+  Favourite.create!(
+    user: user,
+    beer: Beer.all.sample
+    )
 end
 
 
