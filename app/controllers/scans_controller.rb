d9ebda9b74f6c8da8bb0cf2c0600a6f9416587cf
ronan querrec <ronan_querrec@yahoo.com>
2019-11-27 13:08:31 +0100

    Add scan form

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -5,18 +5,25 @@ class ScansController < ApplicationController
   end
 
   def create
+    uploaded_file = params[:image]
+    tmp_file = Tempfile.new(uploaded_file.original_filename)
+    File.open(tmp_file.path, 'wb') do |file|
+      file.write(uploaded_file.read)
+    end
+    res = Cloudinary::Uploader.upload(tmp_file.path)
+    uploaded_url = res["url"]
+
     @scan_results = []
 
     image_annotator = Google::Cloud::Vision::ImageAnnotator.new
 
     response = image_annotator.text_detection(
-      image: "https://thumbs.dreamstime.com/z/bouteille-de-bi%C3%A8re-punk-de-brewdog-ipa-d-%C3%A9cossais-89494280.jpg",
+      image: uploaded_url,
       max_results: 1 # optional, defaults to 10
     )
 
     response.responses.each do |res|
       res.text_annotations.each do |text|
-
         @scan_results << text.description
       end
     end
