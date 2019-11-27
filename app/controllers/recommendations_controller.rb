4fdbdc9a7b8d3c1fdbba8448b2cda81519a1c9cc
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-27 12:13:36 +0100

    initializing da shit

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,6 +1,18 @@
 class RecommendationsController < ApplicationController
+  before_action :set_beer, only: [:create, :destroy]
+
   def index
     # You'll have to modify this method later
-    @recommendations = Favourite.all
+    @beers = current_user.beers
+  end
+
+  private
+
+  def set_beer
+    @beer = Beer.find(favourite_params[:beer_id])
+  end
+
+  def favourite_params
+    params.require(:favourite).permit(:beer_id)
   end
 end
