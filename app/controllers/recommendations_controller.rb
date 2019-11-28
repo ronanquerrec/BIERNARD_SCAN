ea035e69ae18d06c6a9a53bd08f902c38b685ccd
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-28 10:54:42 +0100

    First reco is now based on the last added favourite's style

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -2,5 +2,6 @@ class RecommendationsController < ApplicationController
   def index
     @gold_beers = Beer.where(colour: "Gold").first(5)
     @sweet_beers = Beer.where("sweetness > ?", 60).first(5)
+    @style_reco = Beer.where(style: Favourite.last.beer.style).first(5)
   end
 end
