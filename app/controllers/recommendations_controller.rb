4cc066f9fa3b38f5088339fe45dfe82c9ade5d0a
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-28 13:19:44 +0100

    moved recommendations in the show

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,10 +1,16 @@
 class RecommendationsController < ApplicationController
   def index
-    @style_reco = Beer.where(style: Favourite.last.beer.style).sample(5)
+    @style_reco = Beer.where(style: Favourite.last.beer.style).sample(10)
     @sweet_beers = Beer.where("sweetness > ?", 60).sample(5)
-    # Reco 2
-    # Manage to select all beers among the favourites
-    # Manage to determine which tags are the most frequent among the favourites
-    # Offer last recommendation based on most frequent beer tags
+    # Base recommendation on most frequent tags
+  end
+
+  # Sort most frequent flavours in favourites
+  def favtag
+    all_tags = []
+    Favourite.all.each do |favourite|
+      all_tags << favourite.beer.flavours.map { |flavour| flavour.name }
+    end
+    all_tags
   end
 end
