48f61fa77a1a9344ce964a4ff85434aecf167084
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-11-28 11:34:40 +0100

    getting closer

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,7 +1,10 @@
 class RecommendationsController < ApplicationController
   def index
-    @gold_beers = Beer.where(colour: "Gold").first(5)
-    @sweet_beers = Beer.where("sweetness > ?", 60).first(5)
-    @style_reco = Beer.where(style: Favourite.last.beer.style).first(5)
+    @style_reco = Beer.where(style: Favourite.last.beer.style).sample(5)
+    @sweet_beers = Beer.where("sweetness > ?", 60).sample(5)
+    # Reco 2
+    # Manage to select all beers among the favourites
+    # Manage to determine which tags are the most frequent among the favourites
+    # Offer last recommendation based on most frequent beer tags
   end
 end
