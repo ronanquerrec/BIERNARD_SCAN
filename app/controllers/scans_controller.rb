0f1ffef14b1483d1b4ee21f343365fa273480fd5
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-02 15:12:44 +0100

    fixed usermedia on safari, video is now full size on android, added better loader, added overlay on video

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,5 +1,7 @@
 # require "google/cloud/vision"
 class ScansController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @page_title = "scan"
   end
---
+++ app/javascript/components/set_vh.js
@@ -0,0 +1,6 @@
+const setVh = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+export { setVh };
