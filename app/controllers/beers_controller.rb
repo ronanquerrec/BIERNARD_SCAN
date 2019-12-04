b2cbae25c7e971a157f4052c2d25c1efa9b02196
ronan querrec <ronan_querrec@yahoo.com>
2019-12-04 11:51:11 +0100

    delete duplicates on show

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,5 +5,6 @@ class BeersController < ApplicationController
     @beer = Beer.find(params[:id])
     @favourite = Favourite.where(user: current_user, beer: @beer).first || Favourite.new
     @show_reco = Beer.where(style: Beer.find(params[:id]).style).sample(10)
+    @show_reco.delete(@beer)
   end
 end
