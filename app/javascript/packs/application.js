ce441973e839dc6a53da0d5d98ccc7e9ac47b0a5
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-04 18:05:54 +0100

    trying animate in application.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
-import "animate.css";
+// import "animate.css";
 import { showVideo } from "../components/video"
 import { dismissNotifListener } from "../components/dismiss_notif"
 import { setVh } from "../components/set_vh"
