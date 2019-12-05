d0277ec11ea17b1f4696c049c088ff702799c442
Jules Bayon de Noyer <julesbn@hotmail.fr>
2019-12-05 17:38:12 +0100

    search-result front

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -8,18 +8,25 @@ const autocomplete = () => {
       })
       .then(response => response.json())
       .then((data) => {
-        const searchResult = document.querySelector('.search-result');
-        searchResult.innerHTML = ''
-        data.forEach((beer) => {
-          searchResult.insertAdjacentHTML('beforeend', `<li class="suggestion" data-name="${beer['name']}">${beer['name']}</li>`)
-        });
-        const suggestions = document.querySelectorAll('.suggestion')
-        suggestions.forEach((beer) => {
-         beer.addEventListener("click", (event) => {
-          input.value = beer.dataset.name
+        if (data.length > 0) {
+          const searchResult = document.querySelector('.search-result');
           searchResult.innerHTML = ''
-         });
-        });
+          data.forEach((beer) => {
+            searchResult.insertAdjacentHTML('beforeend', `<li class="suggestion" data-name="${beer['name']}">${beer['name']}</li>`)
+          });
+          const suggestions = document.querySelectorAll('.suggestion')
+          suggestions.forEach((beer) => {
+           beer.addEventListener("click", (event) => {
+            input.value = beer.dataset.name
+            searchResult.innerHTML = ''
+           });
+          });
+        }
+        else {
+          const searchResult = document.querySelector('.search-result');
+          searchResult.innerHTML = ''
+          searchResult.insertAdjacentHTML('beforeend', `<li class="suggestion-not-found">Je ne connais pas cette bière.</li>`)
+        }
       });
     });
   }
