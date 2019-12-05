12eda1acc35c10aae27f9bc4ddfeedc10466f70b
Rémi Chatenay <remi.chatenay@gadz.org>
2019-12-05 13:00:40 +0100

    added required sign in and refacto navbar html and css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: :home
 
-  def home
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 end
